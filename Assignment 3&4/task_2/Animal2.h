f28ab1481fdce27e9401a93ba5dd998e56983399
David (DongYun)  Kim <36246244+dav1dk1m@users.noreply.github.com>
2021-12-28 17:14:39 -0600

    Add files via upload

---
+++ Assignment 3&4/task_2/Animal2.h
@@ -0,0 +1,69 @@
+// Name: DongYun Kim
+// Student Number: 200405213
+// Date: 2021-11-14
+// CS115-Assignment 3&4_Task Two
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Task Two: Operator Overloading
+// implement an assignment operator “=” overloading in the Cat class.
+
+class Animal
+{
+  public:
+  //Getters
+  string get_name()
+  {
+    return name_;
+  }
+  double get_weight()
+  {
+    return weight_;
+  }
+// Getter function for color and return back color_;
+  string get_color()
+  {
+    return color_;
+  }
+
+  protected: // Accessible by Animal and classes that inherit it
+  //constructor
+  Animal(string name,double weight, string color): name_{name}, weight_{weight}, color_{color}
+  {
+    cout << "Animal Constructor" << endl;
+  }
+
+  //Member vars
+  string name_;
+  double weight_;
+  string color_;
+};
+
+// Inheritance-Derived-class: Cat; Base-class: Animal
+class Cat: public Animal
+{
+  public:
+  //Cat Constructor
+  Cat(string name,double weight, string color): Animal{name, weight, color}
+  {
+    cout << "Cat Constructor" << endl;
+  }
+
+  // Method
+  void meow()
+  {
+    cout << "meow meow" << endl;
+  }
+
+// using assignment "=" overloading in the Cat class so that it allows
+// to create new Cat instances from existing objects.
+  void operator=(const Cat& c)
+  {
+    name_ = c.name_;
+    weight_ = c.weight_;
+    color_ = c.color_;
+  }
+
+};
---
+++ Assignment 3&4/task_2/main2.cpp
@@ -0,0 +1,26 @@
+// Name: DongYun Kim
+// Student Number: 200405213
+// Date: 2021-11-14
+// CS115-Assignment 3&4_Task Two
+
+// Task Two: Operator Overloading
+// implement an assignment operator “=” overloading in the Cat class.
+
+#include "Animal2.h"
+
+int main()
+{
+  Cat cat1 {"Inheritance", 115.0, "White"}; //Cat name Inheritance and weight 115.0
+  cout << cat1.get_name() << ", " << cat1.get_weight() <<  ", " << cat1.get_color() << endl;
+  cat1.meow(); //invoke the member function meow
+
+  Cat cat2 = cat1;
+  cout << cat2.get_name() << ", " << cat2.get_weight() << ", " << cat2.get_color() << endl; //print out same as cat1
+  cat2.meow(); //invoke the member function meow
+
+  Cat cat3 = cat2;
+  cout << cat3.get_name() << ", " << cat3.get_weight() << ", " << cat3.get_color() <<endl; //print out same as cat1 and cat2
+  cat3.meow(); //invoke the member function meow
+
+  return 0;
+}
