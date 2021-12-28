26a6f7cb62867cfd6a5a69e83241414c0fcadc59
David (DongYun)  Kim <36246244+dav1dk1m@users.noreply.github.com>
2021-12-28 17:16:05 -0600

    Add files via upload

---
+++ Assignment 3&4/task_3/Animal3.h
@@ -0,0 +1,91 @@
+// Name: DongYun Kim
+// Student Number: 200405213
+// Date: 2021-11-14
+// CS115-Assignment 3&4_Task Three
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Task Three: Template Function
+// add template operator “=” overloading function outside the Cat class. This
+// template function uses generic data types such as Cat and Dog as the input parameter data
+// types.
+
+// implement template function and call it T
+
+class Animal
+{
+  public:
+  //Getters
+  string get_name()
+  {
+    return name_;
+  }
+  double get_weight()
+  {
+    return weight_;
+  }
+// Getter function for color and return back color_;
+  string get_color()
+  {
+    return color_;
+  }
+
+  // implement template member function inside Animal class and call it T
+  template <typename T>
+  void operator=(const T& o)
+  {
+    name_ = o.name_;
+    weight_ = o.weight_;
+    color_ = o.color_;
+  }
+
+
+  protected: // Accessible by Animal and classes that inherit it
+  //constructor
+  Animal(string name,double weight, string color): name_{name}, weight_{weight}, color_{color}
+  {
+    cout << "Animal Constructor" << endl;
+  }
+
+  //Member vars
+  string name_;
+  double weight_;
+  string color_;
+};
+
+// Inheritance-Derived-class: Cat; Base-class: Animal
+class Cat: public Animal
+{
+  public:
+  //Cat Constructor
+  Cat(string name,double weight, string color): Animal{name, weight, color}
+  {
+    cout << "Cat Constructor" << endl;
+  }
+
+  // Method
+  void meow()
+  {
+    cout << "meow meow" << endl;
+  }
+};
+
+// Inheritance-Derived-class: Dog; Base-class: Animal
+class Dog: public Animal
+{
+  public:
+  //Dog Constructor
+  Dog(string name,double weight, string color): Animal{name, weight, color}
+  {
+    cout << "Dog Constructor" << endl;
+  }
+
+  // Method
+  void bark()
+  {
+    cout << "woof woof" << endl;
+  }
+};
---
+++ Assignment 3&4/task_3/main3.cpp
@@ -0,0 +1,38 @@
+// Name: DongYun Kim
+// Student Number: 200405213
+// Date: 2021-11-14
+// CS115-Assignment 3&4_Task Three
+
+// Task Three: Template Function
+
+#include "Animal3.h"
+
+int main()
+{
+  Cat cat1 {"Inheritance", 115.0, "oragne"}; //Cat name Inheritance and weight 115.0
+  cout << cat1.get_name() << ", " << cat1.get_weight() << ", " << cat1.get_color() <<endl;
+  cat1.meow(); //invoke the member function meow
+
+  Cat cat2 = cat1;
+  cout << cat2.get_name() << ", " << cat2.get_weight() << ", " << cat2.get_color() <<endl;//print out same as cat1
+  cat2.meow(); //invoke the member function meow
+
+  Cat cat3 = cat2;
+  cout << cat3.get_name() << ", " << cat3.get_weight() << ", " << cat3.get_color() <<endl; //print out same as cat1 and cat2
+  cat3.meow(); //invoke the member function meow
+
+  Dog dog1 {"CS", 115.0, "black"}; //Dog name CS and weight 115.0
+  cout << dog1.get_name() << ", " << dog1.get_weight() << ", " << dog1.get_color() <<endl;
+  dog1.bark(); //invoke the member function bark
+
+  Dog dog2 = dog1;
+
+  cout << dog2.get_name() << ", " << dog2.get_weight() << ", " << dog2.get_color() <<endl;//print out same as dog1
+  dog2.bark(); //invoke the member function bark
+
+  Dog dog3 = dog2;
+  cout << dog3.get_name() << ", " << dog3.get_weight() << ", " << dog3.get_color() <<endl; //print out same as dog1 and dog2
+  dog3.bark(); //invoke the member function bark
+  
+  return 0;
+}
